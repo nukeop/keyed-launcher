840af84f4b07a135017610ba28838f65bd97d8a3
nukeop <12746779+nukeop@users.noreply.github.com>
2025-06-27 01:27:34 +0200

    Simplified linter config

--- src/stores/launcher.ts
+++ src/stores/launcher.ts
@@ -1,15 +1,15 @@
 import { create } from 'zustand'
 
 interface LauncherState {
-  isVisible: boolean
-  searchQuery: string
-  setVisible: (visible: boolean) => void
-  setSearchQuery: (query: string) => void
+  isVisible: boolean;
+  searchQuery: string;
+  setVisible: (visible: boolean) => void;
+  setSearchQuery: (query: string ) => void;
 }
 
 export const useLauncherStore = create<LauncherState>(set => ({
   isVisible: false,
   searchQuery: '',
-  setVisible: visible => set({ isVisible: visible }),
-  setSearchQuery: query => set({ searchQuery: query }),
+  setVisible: (visible) => set({ isVisible: visible }),
+  setSearchQuery: (query) => set({ searchQuery: query }),
 }))
