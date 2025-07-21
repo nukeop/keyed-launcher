047164c3d3b4922ccad993048d9b49e0042f64cf
nukeop <12746779+nukeop@users.noreply.github.com>
2025-07-22 01:47:04 +0200

    Single instance plugin

--- packages/launcher/src-tauri/src/lib.rs
+++ packages/launcher/src-tauri/src/lib.rs
@@ -11,6 +11,7 @@ mod window;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(
