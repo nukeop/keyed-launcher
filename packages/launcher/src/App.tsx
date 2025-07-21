8170125f07c0edd7021575501bd641644c6380ac
nukeop <12746779+nukeop@users.noreply.github.com>
2025-07-22 01:47:27 +0200

    Dummy component to load tailwind colors for plugins

--- packages/launcher/src/App.tsx
+++ packages/launcher/src/App.tsx
@@ -1,6 +1,7 @@
 import { AppContainer } from './components/AppContainer';
 import { ColorPaletteDebugger } from './components/ColorPaletteDebugger';
 import { CommandPalette } from './components/CommandPalette';
+import { Dummy } from './components/Dummy';
 import { LauncherTransition } from './components/LauncherTransition';
 import { ThemeDebugger } from './components/ThemeDebugger';
 import { isThemeDebuggerVisible } from './hooks/useCommandPaletteResults';
